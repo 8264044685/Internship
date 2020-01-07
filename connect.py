ff3ab8c515bd7f44b38e802454d392dc5af47905
Dabhi <parasdabhi1996@gmail.com>
2020-01-07 17:46:53 +0530

    add comments

--- connect.py
+++ connect.py
@@ -6,18 +6,28 @@ mydb = myclient['BankDB']
 # print("collection are",mydb.list_collection_names())
 # paras
 colList= mydb.list_collection_names()
+
+
 collection = mydb['account']
+
+#Count number of record in collection
 total= collection.count()
-# print("Total record  is :",total)
+
+#Function for increment 1 value in id column in collection
 def sequence(id):
     data=total
     data += 1
     return data
+#end of increment function
+
+
+#Check colletion name 'account' is exists or not
 if "account" in colList:
     print("collection already exists")
 else:
     myTable = mydb['account']
 
+# Display diferent message in promt
 print("0. Exit")
 print("1. Insert")
 print("2. Update")
@@ -26,15 +36,13 @@ print("4. Delete")
 print("5. Withdraw")
 print("6. Diposit")
 
-
+# Function for fetch all the record from 'account' collection
 def getAllData():
-
     for x in collection.find():
 
         print(x)
 
-
-
+# Function for insert data into 'account' collection
 def insert_data(func,name,amount,withdraw,diposit):
     try:
         data = func(id)
@@ -49,6 +57,7 @@ def insert_data(func,name,amount,withdraw,diposit):
     except Exception as e:
         print(e)
 
+# Function for Delete record
 def delete_data(id):
     myquery = {'id': id}
     delete = collection.delete_one(myquery)
@@ -57,6 +66,7 @@ def delete_data(id):
     else:
         print("Error")
 
+# Function for Update record
 def update_data(id):
     print(id)
     name = input("Enter name")
@@ -65,6 +75,8 @@ def update_data(id):
     # collection.update_one(myquery,newvalues)
     collection.update_one({'id':id},{'$set':{'name':name}})
 
+
+# Function for withdrawl money
 def withdraw(id):
     money = int(input("Withdrawal amount"))
     data = collection.find_one({"id":id})
@@ -73,6 +85,8 @@ def withdraw(id):
     collection.update_one({'id': id}, {'$set': {'amount': amount,'withdraw':money}})
     updated_data=collection.find_one({"id": id})
     print(updated_data)
+
+# Funtion for diposit money
 def diposit(id):
     money = int(input("Withdrawal amount"))
     data = collection.find_one({"id": id})
@@ -82,6 +96,7 @@ def diposit(id):
     updated_data = collection.find_one({"id": id})
     print(updated_data)
 
+# Code for different choices. It's break when user enter zero.
 choice = 1
 while choice > 0:
     choice = int(input("Enter Your Choice"))
