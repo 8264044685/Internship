3c67e8e6366fe17c21db4180cb199f4a75f143c5
Dabhi <parasdabhi1996@gmail.com>
2020-01-08 11:12:01 +0530

    changed code

--- connect.py
+++ connect.py
@@ -7,18 +7,23 @@ mydb = myclient['BankDB']
 # paras
 colList= mydb.list_collection_names()
 
-
 collection = mydb['account']
 
 #Count number of record in collection
-total= collection.count()
-print("toal record is :" ,total)
+# total1 = collection.count()
+count = 0
+for total1 in collection.find():
+    count +=1
+
+
+# print("toal record is :" ,total1)
 #Function for increment 1 value in id column in collection
 def sequence(id):
-    data=total
-    data += 1
+    data=count
+
+    data +=1
     return data
-#end of increment function
+3#end of increment function
 
 
 #Check colletion name 'account' is exists or not
@@ -39,8 +44,12 @@ print("6. Diposit")
 # Function for fetch all the record from 'account' collection
 def getAllData():
     for x in collection.find():
+        print("Id :",x['id'])
+        print("Name :",x['name'])
+        print("Amount :",x['amount'])
+        print("*" * 40)
 
-        print(x)
+
 
 # Function for insert data into 'account' collection
 def insert_data(func,name,amount,withdraw,diposit):
@@ -59,51 +68,84 @@ def insert_data(func,name,amount,withdraw,diposit):
 
 # Function for Delete record
 def delete_data(id):
-    myquery = {'id': id}
-    delete = collection.delete_one(myquery)
-    if delete:
-        print("Delete successfully")
+    id1 = collection.find_one({"id": id})
+    if id == id1['id']:
+
+        myquery = {'id': id}
+        delete = collection.delete_one(myquery)
+        if delete:
+            print("Delete successfully")
+        else:
+            print("Error")
     else:
-        print("Error")
+        print("Id not found")
 
 # Function for Update record
 def update_data(id):
-    print(id)
-    name = input("Enter name")
-    # myquery = {"id": id}
-    # newvalues = {"$set": {"name": name}}
-    # collection.update_one(myquery,newvalues)
-    collection.update_one({'id':id},{'$set':{'name':name}})
-
+    # print("update id is ",id)
+    id1 = collection.find_one({"id": id})
+    # print("find id is :",id1['id'])
+    if id == id1['id']:
+        name = input("Enter name")
+        # myquery = {"id": id}
+        # newvalues = {"$set": {"name": name}}
+        # collection.update_one(myquery,newvalues)
+        collection.update_one({'id':id},{'$set':{'name':name}})
+    else:
+        print("ID account not found")
 
 # Function for withdrawl money
 def withdraw(id):
-    money = int(input("Withdrawal amount"))
-    data = collection.find_one({"id":id})
-    amount = data['amount']
-    amount -= money
-    collection.update_one({'id': id}, {'$set': {'amount': amount,'withdraw':money}})
-    updated_data=collection.find_one({"id": id})
-    print(updated_data)
 
+    data = collection.find_one({"id":id})
+    if data == id:
+        money = int(input("Withdrawal amount"))
+        amount = data['amount']
+        if money <= 0:
+           print("Please Enter valid amount")
+        elif amount >= money:
+            updated_data = collection.find_one({"id": id})
+            amount -= money
+            collection.update_one({'id': id}, {'$set': {'amount': amount,'withdraw':money}})
+            print('ID : ',updated_data['id'])
+            print("Name :", updated_data['name'])
+            print("Amount :",updated_data['amount'])
+            print("*" * 40)
+        else:
+            print("\n Insufficient balance  ")
+    else:
+        print("Id account is not found")
 # Funtion for diposit money
 def diposit(id):
-    money = int(input("Withdrawal amount"))
-    data = collection.find_one({"id": id})
-    amount = data['amount']
-    amount += money
-    collection.update_one({'id': id}, {'$set': {'amount': amount, 'diposit': money}})
     updated_data = collection.find_one({"id": id})
-    print(updated_data)
+
+    if update_data == id:
+
+        money = int(input("Enter an amount : "))
+
+        data = collection.find_one({"id": id})
+        amount = data['amount']
+        if money <= 0:
+            print("Please enter valid balance ")
+        else :
+            amount += money
+            collection.update_one({'id': id}, {'$set': {'amount': amount, 'diposit': money}})
+
+            print('ID : ', updated_data['id'])
+            print("Name :", updated_data['name'])
+            print("Amount :", updated_data['amount'])
+            print("*" * 40)
+    else:
+        print("Id account not found")
 
 # Code for different choices. It's break when user enter zero.
 choice = 1
 while choice > 0:
-    choice = int(input("Enter Your Choice"))
+    choice = int(input("Enter Your Choice : "))
     if choice == 1:
 
-        name = input("Enter your name")
-        amount = int(input("Enter Bank balance"))
+        name = input("Enter your name : ")
+        amount = int(input("Enter Bank balance : "))
         withdraw = 0
         diposit = 0
         insert_data(sequence,name,amount,withdraw,diposit)
@@ -114,14 +156,14 @@ while choice > 0:
     elif choice == 3:
         getAllData()
     elif choice == 4:
-        id = int(input("Enter a customer id To delete record"))
+        id = int(input("Enter a customer id :"))
         delete_data(id)
         getAllData()
     elif choice == 5:
-        id = int(input("Enter a customer id withdrawal amount : "))
+        id = int(input("Enter a customer id : "))
         withdraw(id)
     elif choice== 6:
-        id = int(input("Enter a customer id withdrawal amount : "))
+        id = int(input("Enter a customer id  : "))
         diposit(id)
     elif choice == 0:
         break
